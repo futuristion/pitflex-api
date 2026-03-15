7fc4c73f3a672805a5caf97cfa2dc569cdcfd6ee
anderson <futuristion@gmail.com>
2026-03-15 16:11:01 -0300

    ajuste do note pra vps

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import health, ServiceViewSet, ProfessionalViewSet, CustomerViewSet, OrderViewSet
+from .views import landing
 
 router = DefaultRouter()
 router.register("services", ServiceViewSet, basename="services")
@@ -9,6 +10,7 @@ router.register("customers", CustomerViewSet, basename="customers")
 router.register("orders", OrderViewSet, basename="orders")
 
 urlpatterns = [
+    path("", landing, name="landing"),
     path("health/", health),
-    path("api/", include(router.urls)),
+    path("api/", include(router.urls)),    
 ]
